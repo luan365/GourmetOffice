8d8613c2fa645457b050b98e1a22c3aeecfd73a3
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2024-10-22 19:01:59 -0300

    Definindo menuPrincipal como Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Route,Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import Layout  from './Layout';
 
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
 
   return (
     <Routes>
-      <Route index element={<IndexPage/>}/>
-      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<IndexPage/>}/>
+        <Route path="/login" element={<LoginPage />}/>
+      </Route>
     </Routes>
 
   )
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,7 +1,8 @@
-import Header from "../Header";
+
 
 export default function IndexPage(){
     return(
-        <Header/>
+        
+        <div>indexPage vem aqui</div>
     );
 }
