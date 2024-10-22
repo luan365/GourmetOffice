3c3d845ae8d1ee9dd830aca48624fef6b4e4ed49
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2024-10-22 19:52:35 -0300

    LoginPage Style

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -9,17 +9,17 @@ export default function Header(){
               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
             </svg>
             <span className='font-bold text-xl'>
-              GourmetOffice
+              Gourmet
             </span>
           </a>
-          <div className='flex gap-2 border border-gray-500 rounded-full py-2 px-4  shadow-md shadow-black-300'>
-          <div>Estado</div>
-          <div className='border border-l border-gray-500'></div>
-          <div>Cidade</div>
-          <div className='border border-l border-gray-500'></div>
-          <div>Data</div>
+            <div className="flex gap-2 border border-gray-500 rounded-full py-2 px-4 shadow-md shadow-black-300">
+            <div className="px-4">Estado</div>
+            <div className="border border-purple-150"></div>
+            <div className="px-4">Cidade</div>
+            <div className="border border-purple-150"></div>
+            <div className="px-4">Data</div>
           <button className='bg-purple-700 text-white p-1  rounded-full'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
           </button>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,15 @@
 export  default function LoginPage(){
-    return "login vai aparecer aqui"
+    return(
+       <div className="mt-4 ">
+    
+        <h1 className="text-4xl text-center">Login</h1>
+        <form className="max-w-md mx-auto ">
+            <input type="email"placeholder='your@rmail.com'></input>
+            <input type = "password" placeholder="password"/>
+            <button>Login</button>
+
+        </form>
+    </div> 
+    )
+    
 }
