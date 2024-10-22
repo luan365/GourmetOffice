3b83786bf90bffdf2d80a55196a60dfc55aa8f89
Bruno Savoia <brunotsavoia@gmail.com>
2024-10-22 15:49:05 -0300

    criando tela login
    tela de login crua

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import TelaLogin from './TelaLogin.jsx';
+import './main.css';
 
+// Se quiser usar uma imagem, pode definir aqui
+const backgroundImage = 'path/to/your/image.png';
+//const backgroundColor = cor
+
+
+//se quiser cor no lugar de imagem, so colocar "<App backgroundColor={backgroundColor} />"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <TelaLogin backgroundImage={backgroundImage} />
   </StrictMode>,
-)
+);
