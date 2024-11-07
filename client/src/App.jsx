b387d883f980d223e6bd3c064c2af9d2f2958727
Xx-VitorBigV-xX <129459463+Xx-VitorBigV-xX@users.noreply.github.com>
2024-11-07 17:05:02 -0300

    frontend + axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Route,Routes} from "react-router-dom";
 import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage';
 import Layout  from './Layout';
 
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<IndexPage/>}/>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path='/register'element={<RegisterPage/>}/>
       </Route>
     </Routes>
 
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [senha, setSenha] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [telefone, setTelefone] = useState("");
+  async function registerUser(ev){
+    ev.preventDefault();
+    try{
+      await axios.put('http://localhost:8080/insertEmpresa',{
+        nome,
+        senha,
+        email,
+        endereco,
+        cnpj,
+        telefone,
+      });
+      alert('registro deu erto');
+    }catch(e){
+      alert('registro falhou')
+    }
+  }
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center">Registra-se</h1>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="nome"
+            title="Apenas letras são permitidas"
+            value={nome}
+            onChange={(ev)=>setNome(ev.target.value)}
+          />
+          <input 
+          type="email" 
+          placeholder="your@rmail.com"
+          value={email}
+          onChange={(ev)=>setEmail(ev.target.value)}
+          />
+          <input type="password"
+           placeholder="senha"
+           value={senha}
+           onChange={(ev)=>setSenha(ev.target.value)}
+            />
+          <input type="text"
+           placeholder="cnpj" 
+           pattern="\d*" 
+           maxLength={14}
+           value={cnpj}
+           onChange={(ev)=>setCnpj(ev.target.value)}
+           />
+          <input
+            type="text"
+            placeholder="tel:(xx)xxxxx-xxxx"
+            pattern="\d*"
+            maxLength={11}
+            value={telefone}
+            onChange={(ev)=>setTelefone(ev.target.value)}
+          />
+          <input type="text" 
+           placeholder="Endereço" 
+           value={endereco}
+           onChange={(ev)=>setEndereco(ev.target.value)}
+          />
+          <button className="text-white">Registrar</button>
+          <div className="text-center py-2">
+            Já possui uma conta?
+            <Link className="underline font-bold" to={"/login"}>
+              entre em Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
