2be1663a7dda720ee7592a77c495101112de5d6c
Bruno Savoia <brunotsavoia@gmail.com>
2024-11-14 09:01:04 -0300

    escolha entre empresa e cozinha registro

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -10,22 +10,52 @@ export default function RegisterPage() {
   const [endereco, setEndereco] = useState("");
   const [cnpj, setCnpj] = useState("");
   const [telefone, setTelefone] = useState("");
+  const [cozinha, setCozinha] = useState(false);
+  const [empresa, setEmpresa] = useState(false);
   async function registerUser(ev){
     ev.preventDefault();
     try{
-      await axios.put('http://localhost:8080/insertEmpresa',{
-        nome,
-        senha,
-        email,
-        endereco,
-        cnpj,
-        telefone,
-      });
-      alert('registro deu certo');
+
+      if(cozinha==true){
+        await axios.put('http://localhost:8080/insertCozinha',{
+          nome,
+          senha,
+          email,
+          endereco,
+          cnpj,
+          telefone,
+        });
+        alert('registro deu certo');
+      }
+
+      if(empresa==true){
+        await axios.put('http://localhost:8080/insertEmpresa',{
+          nome,
+          senha,
+          email,
+          endereco,
+          cnpj,
+          telefone,
+        });
+        alert('registro deu certo');
+      }
+
+
     }catch(e){
-      alert('registro falhou')//adicionar possiveis erros, esse email já esta em uso por exemplo
+      alert('registro falhou' + e)//adicionar possiveis erros, esse email já esta em uso por exemplo
     }
   }
+
+  const handleCheckboxChange = (type) => {
+    if (type === "cozinha") {
+      setCozinha(true);
+      setEmpresa(false);
+    } else {
+      setCozinha(false);
+      setEmpresa(true);
+    }
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-around mt-20">
       <div className="mb-64">
@@ -69,6 +99,24 @@ export default function RegisterPage() {
            value={endereco}
            onChange={(ev)=>setEndereco(ev.target.value)}
           />
+          <div className="flex items-center space-x-4 mt-4">
+            <label>
+              <input
+                type="checkbox"
+                checked={cozinha}
+                onChange={() => handleCheckboxChange("cozinha")}
+              />
+              Cozinha
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                checked={empresa}
+                onChange={() => handleCheckboxChange("empresa")}
+              />
+              Empresa
+            </label>
+          </div>
           <button className="text-white">Registrar</button>
           <div className="text-center py-2">
             Já possui uma conta?
